85c541fe6c9a3184d27b91a60b8048473077a3f7
Michael Costa <michaelmcosta@me.com>
2016-01-02 04:19:47 -0500

    asdf

--- src/testwidget.js
+++ src/testwidget.js
@@ -24,7 +24,7 @@ angular.module('adf.widget.iframe', ['adf.provider'])
   }]);
 
 angular.module("adf.widget.iframe").run(["$templateCache", function($templateCache) {$templateCache.put("{widgetsPath}/iframe/src/edit.html","<form role=form><div class=form-group><label for=url>URL</label> <input type=url class=form-control id=url ng-model=config.url placeholder=http://www.example.com></div><div class=form-group><label for=url>Height</label> <input type=text class=form-control id=url ng-model=config.height></div></form>");
-$templateCache.put("{widgetsPath}/iframe/src/view.html","<div><div class=\"alert alert-info\" ng-if=!config.url>Please insert a url in the widget configuration</div><iframe ng-if=iframe.url class=adf-iframe style=\"height: {{config.height}};width:100%;\" src={{iframe.url}}></iframe></div>");}]);
+$templateCache.put("{widgetsPath}/iframe/src/view.html","<div class=\"expand\"><div class=\"alert alert-info\" ng-if=!config.url>Please insert a url in the widget configuration</div><iframe ng-if=iframe.url class=\"adf-iframe expand\" style=\"min-height:500px;height: {{config.height}};width:100%;\" src={{iframe.url}} seamless allowfullscreen></iframe></div>");}]);
 
 
 
