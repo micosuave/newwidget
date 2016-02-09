74c6ac0ebefd79f6c673e963455f22707ea0b292
Michael Costa <michael@mylionlaw.com>
2016-02-09 16:17:24 -0500

    u

--- src/testwidget.js
+++ src/testwidget.js
@@ -11,6 +11,7 @@ angular.module('adf.widget.frame', ['adf.provider'])
         frameless: true,
         reload: true,
         edit: {
+            controller: 'iframeController',
           templateUrl: '{widgetsPath}/iframe/src/edit.html'
         },
         config: {
@@ -18,13 +19,14 @@ angular.module('adf.widget.frame', ['adf.provider'])
         }
       });
   }])
-  .controller('iframeController', ["$sce", "config","ckdefault", function($sce, config, ckdefault){
+  .controller('iframeController', ["$sce", "config","ckdefault","$scope", function($sce, config, ckdefault, $scope){
     if (config.url){
       this.url = $sce.trustAsResourceUrl(config.url);
     }
     if (config.srcdoc){
         this.srcdoc = $sce.trustAsHtml(config.srcdoc);
     }
+    $scope.ckdefault = ckdefault;
   }]);
 
 angular.module("adf.widget.frame").run(["$templateCache", function($templateCache) {$templateCache.put("{widgetsPath}/iframe/src/edit.html","<form role=form><div class=form-group><label for=url>URL</label> <input type=url class=form-control id=url ng-model=config.url placeholder=http://www.example.com></div><div class=form-group><label for=url>Height</label> <input type=text class=form-control id=url ng-model=config.height><textarea ckeditor=ckdefault ng-model=config.srcdoc ng-model-options=\"{updateOn: 'default blur', debounce: {'default': 20000, 'blur': 0}}\"</div></form>");
