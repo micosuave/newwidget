e6aade68a2d7020415642f7f8c5d40271c68b55b
Michael Costa <michaelmcosta@me.com>
2016-08-18 22:08:20 -0400

    u

--- src/testwidget.js
+++ src/testwidget.js
@@ -465,7 +465,7 @@ angular.module('adf.widget.testwidget', ['adf.provider', 'pdf', 'firebase', 'ui.
       // draft.$loaded().then(function(drat){
       //   $scope.b = angular.copy(drat);
       // });
-
+      $http.get({url: '/files/uploads/'+draft.$id+'.html'}).then(function(resp){$scope.draft.content = resp.data;$scope.b.content = resp.data;});
       if ($state.includes('composer')) {
         $scope.inlab = true
       }
